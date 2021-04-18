79b0b7e6d0d3c2da0c90d891e24123b49d5df56a
Nata <merlincat.no@gmail.com>
2021-04-18 19:36:23 +0300

    add exam without responsive

--- exam/assets/js/main.js
+++ exam/assets/js/main.js
@@ -1,26 +1,29 @@
-// $(function(){
-//     $(window).scroll(function(){
-//         if($(window).scrollTop()>70){
-//             $("header").addClass("fixed");
-//         }else{
-//             $("header").removeClass("fixed");
-//         }
-//     });
-//     $(".hamburger, #nav_shadow").click(function(){
-//         $(".hamburger").toggleClass("is-active");
-//         $("body").toggleClass("open");
-//     })
-//     $(".m_list_item-link").click(function(){
-//         $(".hamburger").removeClass("is-active");
-//         $("body").removeClass("open");
-//     });
-//     $(".m_list_item-link, list_item-link").click(function(e){
-//         e.preventDefault();
-//         let top = $($(this).attr("href")).offset().top;
-//         $("html, body").animate({scrollTop:top}, 400);
-//     });
-//     $("#to_top").click(function(){
-//         $("html, body").animate({scrollTop:0}, 400);
-//     })
-//     // $("input").styler();
-// });
+$(function(){
+    // $(window).on('scroll', function(){
+    //     if($(window).scrollTop()>70){
+    //         $("header").addClass("fixed");
+    //     }else{
+    //         $("header").removeClass("fixed");
+    //     }
+    // });
+    // $(".hamburger, #nav_shadow").on('click', function(){
+    //     $(".hamburger").toggleClass("is-active");
+    //     $("body").toggleClass("open");
+    // })
+    $(".hamburger").on('click', function(){
+        $(".hamburger").toggleClass("is-active");
+        $("body").toggleClass("open");
+    })
+    // $(".m_list_item-link").on('click',function(){
+    //     $(".hamburger").removeClass("is-active");
+    //     $("body").removeClass("open");
+    // });
+    // $(".m_list_item-link, list_item-link").on('click', function(e){
+    //     e.preventDefault();
+    //     let top = $($(this).attr("href")).offset().top;
+    //     $("html, body").animate({scrollTop:top}, 400);
+    // });
+    // $("#to_top").on('click', function(){
+    //     $("html, body").animate({scrollTop:0}, 400);
+    // })
+});
